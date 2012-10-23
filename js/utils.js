e0ebc81dce12465668a0bc07cbdf0bd6b7cfe415
Vorg <marcin.ignac@gmail.com>
2012-10-23 10:58:51 +0100

    Node editing

--- js/utils.js
+++ js/utils.js
@@ -7,3 +7,21 @@ String.prototype.format = String.prototype.f = function() {
     }
     return s;
 };
+
+function selectElementText(el) {
+  if (window.getSelection && document.createRange) {
+    var range = document.createRange();
+    if (el.childNodes.length > 0) {
+      range.setStart(el.childNodes[0], el.childNodes[0].length);
+      range.setEnd(el.childNodes[0], el.childNodes[0].length);
+    }
+    else {
+      range.setStart(el, 0);
+      range.setEnd(el, 0);
+    }
+
+    var sel = window.getSelection();
+    sel.empty();
+    sel.addRange(range);
+  }
+}
